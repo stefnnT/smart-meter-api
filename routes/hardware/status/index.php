c7dfb446bac46d6e7a18363672452027220e0645
Stephen <stephensolomon48@gmail.com>
2021-04-05 21:12:44 +0100

    edit to status updatev6

--- routes/hardware/status/index.php
+++ routes/hardware/status/index.php
@@ -24,6 +24,25 @@
       if (isset($_POST['status']) && $_POST['status']) {
         
         $data = $_POST['status'];
+        
+        // foreign code
+        $url = 'https://powercase.natterbase.com/powercheck/user/stats/hook/00017';
+        $data = array('key1' => 'value1', 'key2' => 'value2');
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => "status=".$data
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+
+        // foreign code ends here
+
+
         // $data = "11110|230|49.9|12345|12345.7|100";
         // echo $data;
         
